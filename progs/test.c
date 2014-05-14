7ad4f84e5dc0a946662bb9508c2d954b792a4140
Qiu Zhe <phoeagon@gmail.com>
2014-05-14 11:42:55 +0800

    working

--- progs/test.c
+++ progs/test.c
@@ -3,21 +3,21 @@ unsigned int c , b , a , mode ;
 //int t=1;
 //int b;
 int test(){
-	//unsigned int c , b , a , mode ;
+	unsigned int c , b , a , mode ;
 	// *(unsigned int*)0x84=0x0000aaaa;
 	// unsigned int a = ( *(unsigned int*) 0xC0 ) & 0xffff;
 // *(unsigned int*)0x84=a;
 	//*(unsigned int*) 0x88 = 11;
 	//*(unsigned int*) 0x8c = 12;
 	while(1){
-		//a = ( *(unsigned int*) 0xC0 ) ;
-		//b = ( a / 16 )& 0xf;
-		//c = ( a ) & 0xf ; 
+		a = ( *(unsigned int*) 0xC0 ) ;
+		b = ( a / 16 )& 0xf;
+		c = ( a ) & 0xf ; 
 		//b = 32 ;
 		//c = 16;
 		//mode = 14;
-		b = *(unsigned int*) 0xc8 ;
-		c = *(unsigned int*) 0xcc ;
+		//b = *(unsigned int*) 0xc8 ;
+		//c = *(unsigned int*) 0xcc ;
 		//mode = *(unsigned int*) 0xd0 ; //; ( a / 256 ) & 1;
 		//unsigned int b = *(unsigned int*)0xc8;
 		//unsigned int c = *(unsigned int*)0xcc;
