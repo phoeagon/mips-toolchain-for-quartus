b2d6719285537f36005789c8554d44d4ada593b9
Qiu Zhe <phoeagon@gmail.com>
2014-05-26 16:22:26 +0800

    every function

--- progs/test.c
+++ progs/test.c
@@ -1,63 +1,35 @@
-
-int c  , b , a , mode ;
-//int t=1;
-//int b;
-/*
- * int test(){
-	while(1){
-		//*(unsigned int*) 0x80 = 0xaaaaaaaa ;
-		//*(unsigned int*) 0x8c = 0x55555555 ;
-		*(unsigned int*) 0x8c = 1 ;
-		*(unsigned int*) 0x88 = 2 ;
-		*(unsigned int*) 0x90 = 14 ;
-	}
-}
-*/
+int c , b , a , d , mode ;
 int test(){
 	while(1){
-		c = *(unsigned int*) 0xC8 ;
-		b = *(unsigned int*) 0xCC ;
-		*(unsigned int*) 0x8c = b + c ;
-		*(unsigned int*) 0x88 = b ;
+		a = ( *(unsigned int*) 0xc0 );
+		b = ( a / 16 ) & 0xf;
+		c = (a) & 0xf ;
+		mode = ( a / 256 ) & 0xf ;
+		if ( mode == 0 )  //add
+			*(unsigned int*) 0x8c = b + c ;
+		else if ( mode == 1 ) //multi
+			*(unsigned int*) 0x8c = b * c ;
+		else if ( mode == 2 ){
+			//GCD
+			a = b ; d = c ;
+			while ( a && d ) {
+				if ( a > d ) a -= d;
+				else d -= a ;
+			}
+			*(unsigned int*) 0x8c = a | d ;
+		}else { //binomial coeff
+			a = 1 ;
+			if ( c<b )
+				*(unsigned int*)0x8c = 0;
+			else{
+				for ( d = b + 1 ; d <= c ; ++d )
+					a *= d;
+				for ( d = 1; d <= c-b ; ++d )
+					a /= d;
+				*(unsigned int*) 0x8c = a ;
+			}
+		}
+		*(unsigned int*) 0x88 = b;
 		*(unsigned int*) 0x90 = c ;
 	}
 }
-/*
-int test(){
-	for ( a = 1; a<10 ;++a){
-		c += a;
-	}
-	while (1)
-		*(unsigned int*) 0x90 = c ;
-}
-*/
-/*
-int test(){
-	//unsigned int c , b , a , mode ;
-	// *(unsigned int*)0x84=0x0000aaaa;
-	// unsigned int a = ( *(unsigned int*) 0xC0 ) & 0xffff;
-// *(unsigned int*)0x84=a;
-	//*(unsigned int*) 0x88 = 11;
-	//*(unsigned int*) 0x8c = 12;
-	while(1){
-		a = ( *(unsigned int*) 0xC0 ) ;
-		b = ( a / 16 )& 0xf;
-		c = ( a ) & 0xf ; 
-		//b = 32 ;
-		//c = 16;
-		mode = ( a / 256 ) & 0xf;
-		//b = *(unsigned int*) 0xc8 ;
-		//c = *(unsigned int*) 0xcc ;
-		//mode = *(unsigned int*) 0xd0 ; //; ( a / 256 ) & 1;
-		//unsigned int b = *(unsigned int*)0xc8;
-		//unsigned int c = *(unsigned int*)0xcc;
-		//*(unsigned int*) 0x8c = b * c;
-		*(unsigned int*) 0x8c = b % c ;
-		*(unsigned int*) 0x88 = b ;
-		*(unsigned int*) 0x90 = c ;
-		//*(unsigned int*) 0x8c = b+c  ;
-		
-	}
-}
-
-*/
